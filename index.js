271e3baa90a1ce619901c67f523750d744c1e299
JMiller7334 <Jacob-Miller@Hotmail.com>
2025-02-05 01:02:50 -0600

    Overall overhaul of api; this is receiving point.

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const port = 8081;
+
+app.use(cors());
+app.use(express.json()); //parse for json
+
+//fetch env variable
+require('dotenv').config();
+const vpsIp = process.env.VPS_IP;
+
+app.post('/send-email', async (req, res) => {
+    try {
+        const { name, email, message } = req.body;
+        const requestData = {
+            name,
+            email,
+            message
+        };
+        //handle contacting server php:
+        const response = await axios.post('http://'+vpsIp+'/portfolio-api/public/email.php', requestData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while sending the email.' });
+    }
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
